a473485bd9cba08f3a70fd5df95cbbbb37354867
Daniel Woodward <16451892+DrBarnabus@users.noreply.github.com>
2026-03-07 10:42:10 +0000

    feat: report cwd to Windows Terminal for pane splitting
    Add precmd hook to emit OSC 9;9 escape sequence so Windows Terminal
    can track the working directory and open split panes in the same cwd.

--- files/zsh/.zshrc.d/windows.zsh
+++ files/zsh/.zshrc.d/windows.zsh
@@ -1,3 +1,8 @@
 # ls colour support
 export LS_COLOR_FLAG='--color'
 alias ls='ls --color'
+
+# Report cwd to Windows Terminal for pane splitting
+autoload -Uz add-zsh-hook
+_wt_osc9_9() { printf '\e]9;9;%s\e\\' "$(cygpath -w "$PWD")" }
+add-zsh-hook precmd _wt_osc9_9
--- files/zsh/.zshrc.d/wsl.zsh
+++ files/zsh/.zshrc.d/wsl.zsh
@@ -19,3 +19,8 @@ export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
 
 # Open URLs in the Windows default browser
 export BROWSER=wsl-open
+
+# Report cwd to Windows Terminal for pane splitting
+autoload -Uz add-zsh-hook
+_wt_osc9_9() { printf '\e]9;9;%s\e\\' "$(wslpath -w "$PWD")" }
+add-zsh-hook precmd _wt_osc9_9
