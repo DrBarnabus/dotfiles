e4e67ad5f8432bc894d10098a3054054a1d7883f
Daniel Woodward <16451892+DrBarnabus@users.noreply.github.com>
2026-03-30 22:44:54 +0100

    fix: work around MSYS2 path conversion breaking fzf-tab trigger

--- files/zsh/.zshrc.d/windows.zsh
+++ files/zsh/.zshrc.d/windows.zsh
@@ -1,2 +1,6 @@
+# MSYS2 converts '/' to the Git install path when passing args to native
+# Windows executables, which breaks fzf-tab's --expect=/ argument.
+zstyle ':fzf-tab:*' continuous-trigger '//'
+
 # Report cwd to Windows Terminal for pane splitting
 _wt_enable_cwd_reporting cygpath
