9663d62bbb80f58cc072e7e19f20a106cf18e5de
Daniel Woodward <110159179+DS-DanielW@users.noreply.github.com>
2026-04-02 19:06:49 +0100

    Add pnpm PATH setup for darwin

--- files/zsh/.zshrc.d/darwin.zsh
+++ files/zsh/.zshrc.d/darwin.zsh
@@ -5,6 +5,13 @@ elif [[ -f /usr/local/bin/brew ]]; then
   eval "$(/usr/local/bin/brew shellenv)"
 fi
 
+# pnpm
+export PNPM_HOME="$HOME/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+
 # .NET
 export DOTNET_ROOT="$HOME/.dotnet"
 export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
